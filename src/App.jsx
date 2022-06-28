aa9b89a774a8a9d7a3d085b76dacec56bf5b91e3
ThomasDCW <thomas.dicola@hotmail.fr>
2022-06-28 14:44:48 +0200

    create counter with Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,10 @@
-let id = 3;
-
-const initialState = [
-  {
-    id: 1,
-    title: 'Enregistrer le tutoriel',
-    completed: false,
-  },
-  {
-    id: 2,
-    title: 'Preparer le tutoriel',
-    completed: true,
-  },
-];
-
-const ADD_TODO_ACTION = 'ADD_TODO_ACTION';
-function TodoReducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_TODO_ACTION:
-      return [...state, { id: id++, completed: false, ...action }];
-    default:
-      return state;
-  }
-}
-const state = TodoReducer(undefined, {});
-const newState = TodoReducer(state, {
-  type: ADD_TODO_ACTION,
-  payload: { title: 'Demo' },
-});
+import Counter from './Counter';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>REDUX</header>
-      {console.log(state, newState)}
+      REDUX
+      <Counter />
     </div>
   );
 }
